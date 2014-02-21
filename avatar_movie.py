950df27ea93b1769bae906d3c28d2c085ec9cde0
Maria McKinley <mariak@mariakathryn.net>
2014-02-20 16:53:19 -0800

    now have a movie showing the avatar position in 2d in the courtyard.

---
+++ avatar_movie.py
@@ -0,0 +1,160 @@
+from __future__ import division
+from direct.showbase.ShowBase import ShowBase
+from direct.showbase.DirectObject import DirectObject
+from panda3d.core import PerspectiveLens, Point3, LineSegs, TransparencyAttrib
+from direct.task import Task
+import pickle
+
+
+class AvatarWorld(DirectObject):
+    def __init__(self):
+        # set to record movie
+        self.record = True
+        movie_name = '../movies/frames/avatar/avatar_1191'
+
+        with open('../movies/data/bananarchy_movie_1') as variable:
+            res = pickle.load(variable)
+            start_time = int(pickle.load(variable))
+            if start_time == 0:
+                message = """This data is for a single frame, please use a time_stamp
+                      rather than zero for the starting time"""
+                raise StartError(message)
+            banana_pos = pickle.load(variable)
+            #print(banana_pos)
+            banana_h = pickle.load(variable)
+            #print(banana_h)
+            gone_bananas = pickle.load(variable)
+            #print(gone_bananas)
+            #print(int(gone_bananas[0][-2:]))
+            avatar_h = pickle.load(variable)
+            #print(avatar_h)
+            avatar_pos = pickle.load(variable)
+            #print('before', avatar_pos)
+            avatar_ht = pickle.load(variable)
+            avatar_pt = pickle.load(variable)
+            banana_ts = pickle.load(variable)
+            eye_data = pickle.load(variable)
+            eye_ts = pickle.load(variable)
+            lfp_data = pickle.load(variable)
+
+        # make zero the start time, change to seconds (from milliseconds)
+        self.avatar_ht = [(float(i) - start_time) / 1000 for i in avatar_ht]
+        self.avatar_pt = [(float(i) - start_time) / 1000 for i in avatar_pt]
+        self.banana_ts = [(float(i) - start_time) / 1000 for i in banana_ts]
+        self.eye_ts = [(float(i) - start_time) / 1000 for i in eye_ts]
+        # and now make it official
+        start_time = 0
+
+        # non-time variables still need to be converted from strings
+        #print(res)
+        resolution = [int(i) for i in res]
+        self.avatar_h = [float(i) for i in avatar_h]
+        self.avatar_pos = [[float(j) for j in i] for i in avatar_pos]
+        #print(gone_bananas[0])
+        # bananarchy data and gobananas data slightly different here
+        if len(gone_bananas[0]) == 7:
+            self.gone_bananas = [int(i[-1:]) for i in gone_bananas]
+        else:
+            self.gone_bananas = [int(i[-2:]) for i in gone_bananas]
+
+        self.lfp_data = [float(i) for i in lfp_data]
+        # Things that can affect camera:
+        # options resolution resW resH
+        base = ShowBase()
+        lens = PerspectiveLens()
+        # Fov is set in config for 60
+        lens.setFov(60)
+        # aspectratio should be same as window size
+        # this was for 800x600
+        # field of view 60 46.8264...
+        # aspect ratio 1.3333
+        movie_res = [800, 600]
+        lens.setAspectRatio(800.0 / 600.0)
+        base.cam.node().setLens(lens)
+        #print('Fov', lens.getFov())
+        #print('Aspect Ratio', lens.getAspectRatio())
+        # set near to be same as avatar's radius
+        lens.setNear(0.1)
+        #print('near camera', lens.getNear())
+
+        # background color doesn't show up anyway
+        #base.setBackgroundColor(115 / 255, 115 / 255, 115 / 255)
+
+        self.last_avt = self.avatar_pos.pop(0)
+        #base.cam.setPos(Point3(points[0], points[1], points[2]))
+        #base.cam.setH(self.avatar_h.pop(0))
+        #self.avatar_ht.pop(0)
+        self.avatar_pt.pop(0)
+
+        bananas = range(len(banana_h))
+        #print(bananas)
+        self.bananaModel = []
+
+        for i in bananas:
+            #print(float(banana_pos[i][0]), float(banana_pos[i][1]), float(banana_pos[i][2]))
+            self.bananaModel.append(base.loader.loadModel('models/ball'))
+            self.bananaModel[i].setPos(
+                Point3(float(banana_pos[i][0]), 25, float(banana_pos[i][1])))
+            #Point3(float(banana_pos[i][0]), float(banana_pos[i][1]), float(banana_pos[i][2])))
+            self.bananaModel[i].setScale(0.5)
+            self.bananaModel[i].setColor(1, 1, 0, 1)
+            self.bananaModel[i].reparentTo(render)
+
+        for i, j in enumerate(self.banana_ts):
+            if j < start_time:
+                #print('stashed')
+                self.bananaModel[self.gone_bananas.pop(i)].stash()
+                self.banana_ts.pop(i)
+
+        if self.record:
+            self.movie_task = base.movie(movie_name, 200, 30, 'png', 4)
+
+        #self.accept("space", base.taskMgr.add, [self.frame_loop, "frame_loop"])
+        self.gameTask = taskMgr.add(self.frame_loop, "frame_loop")
+
+        self.gameTask.last = 0         # Task time of the last frame
+
+        # start the clock at 1 second before the official start so has time to load
+        self.gameTask.game_time = start_time - 100
+
+    def frame_loop(self, task):
+        dt = task.time - task.last
+        task.last = task.time
+        if len(self.avatar_pt) > 0:
+            self.update_avt_p(task.time)
+        if len(self.banana_ts) > 0 and self.banana_ts[0] < task.time:
+            self.update_banana()
+        return task.cont
+
+    def update_avt_p(self, t_time):
+        avt = LineSegs()
+        avt.setThickness(5)
+        avt.setColor(1, 1, 1)
+        group_avatar = []
+        while self.avatar_pt[0] < t_time:
+            if len(self.avatar_pt) == 1:
+                break
+            #print('move')
+            #print(self.avatar_pt[i])
+            group_avatar.append(self.avatar_pos.pop(0))
+            #print points
+            self.avatar_pt.pop(0)
+        #print('positions', group_avatar)
+        if group_avatar:
+            avt.moveTo(self.last_avt[0], 25, self.last_avt[1])
+            self.last_avt = group_avatar[-1]
+            for i in group_avatar:
+                #print(i[0], i[1], i[2])
+                avt.drawTo(i[0], 25, i[1])
+            node = render.attachNewNode(avt.create())
+
+    def update_banana(self):
+        #print(self.banana_ts[0])
+        #print('gone', self.gone_bananas[0])
+        self.bananaModel[self.gone_bananas.pop(0)].stash()
+        self.banana_ts.pop(0)
+
+
+if __name__ == "__main__":
+    AW = AvatarWorld()
+    run()
--- make_movie.py
+++ make_movie.py
@@ -1,13 +1,16 @@
 from __future__ import division
 from direct.showbase.ShowBase import ShowBase
 from direct.showbase.DirectObject import DirectObject
-from panda3d.core import PerspectiveLens, Point3, LineSegs
+from panda3d.core import PerspectiveLens, Point3, LineSegs, TransparencyAttrib
 from direct.task import Task
 import pickle
 
 
 class BananaWorld(DirectObject):
     def __init__(self):
+        # set to record movie
+        self.record = True
+        movie_name = '../movies/frames/bananarchy_1191'
 
         with open('../movies/data/bananarchy_movie_1') as variable:
             res = pickle.load(variable)
@@ -39,9 +42,11 @@ class BananaWorld(DirectObject):
         self.avatar_pt = [(float(i) - start_time) / 1000 for i in avatar_pt]
         self.banana_ts = [(float(i) - start_time) / 1000 for i in banana_ts]
         self.eye_ts = [(float(i) - start_time) / 1000 for i in eye_ts]
+        # and now make it official...
+        start_time = 0
 
         # non-time variables still need to be converted from strings
-        print(res)
+        #print(res)
         resolution = [int(i) for i in res]
         self.avatar_h = [float(i) for i in avatar_h]
         self.avatar_pos = [[float(j) for j in i] for i in avatar_pos]
@@ -71,11 +76,11 @@ class BananaWorld(DirectObject):
         movie_res = [800, 600]
         lens.setAspectRatio(800.0 / 600.0)
         base.cam.node().setLens(lens)
-        print('Fov', lens.getFov())
-        print('Aspect Ratio', lens.getAspectRatio())
+        #print('Fov', lens.getFov())
+        #print('Aspect Ratio', lens.getAspectRatio())
         # set near to be same as avatar's radius
         lens.setNear(0.1)
-        print('near camera', lens.getNear())
+        #print('near camera', lens.getNear())
         #base.cam.setPos(0, 0, 1)
         #print('x', base.win.getXSize())
         #print('y', base.win.getYSize())
@@ -106,7 +111,7 @@ class BananaWorld(DirectObject):
         # need to adjust y position for lfp
         self.lfp_gain = 0.1
         self.lfp_offset = -500
-        self.last_lfp = (self.lfp_data.pop(0) * self.lfp_gain) + self.lfp_offset
+        self.last_lfp = [(self.lfp_data.pop(0) * self.lfp_gain) + self.lfp_offset]
         self.gen_lfp = self.get_data(self.lfp_data)
         self.last_lfp_x = 100
         self.lfp_test = 1
@@ -116,6 +121,49 @@ class BananaWorld(DirectObject):
         base.cam.setH(self.avatar_h.pop(0))
         self.avatar_ht.pop(0)
         self.avatar_pt.pop(0)
+
+        self.set_environment()
+
+        #load bananas
+        # if we are not starting at the beginning of the trial, some of the bananas may
+        # already be gone. Create them, and then stash them, so the index still refers to
+        # the correct banana
+
+        bananas = range(len(banana_h))
+        self.bananaModel = []
+
+        for i in bananas:
+            #print('i', i)
+            self.bananaModel.append(base.loader.loadModel('../goBananas/models/bananas/banana.bam'))
+            self.bananaModel[i].setPos(
+                Point3(float(banana_pos[i][0]), float(banana_pos[i][1]), float(banana_pos[i][2])))
+            self.bananaModel[i].setScale(0.5)
+            self.bananaModel[i].setH(float(banana_h[i]))
+            self.bananaModel[i].reparentTo(render)
+
+        #print('start', start_time)
+        for i, j in enumerate(self.banana_ts):
+            if j < start_time:
+                print('stashed')
+                print(j)
+                self.bananaModel[self.gone_bananas.pop(i)].stash()
+                self.banana_ts.pop(i)
+
+        if self.record:
+            self.movie_task = base.movie(movie_name, 150, 30, 'png', 4)
+
+        #self.accept("space", base.taskMgr.add, [self.frame_loop, "frame_loop"])
+        self.gameTask = taskMgr.add(self.frame_loop, "frame_loop")
+
+        self.gameTask.last = 0         # Task time of the last frame
+
+        # start the clock at 1 second before the official start so has time to load
+        self.gameTask.game_time = start_time - 100
+        #print('start', self.gameTask.game_time)
+        #print('head start', self.avatar_ht[0])
+        #print('increment', (1 / 60) * 1000000)
+
+    def set_environment(self):
 
         terrainModel = base.loader.loadModel('../goBananas/models/towns/field.bam')
         terrainModel.setPos(Point3(0, 0, 0))
@@ -144,39 +192,12 @@ class BananaWorld(DirectObject):
         windmillModel.setH(45)
         windmillModel.reparentTo(render)
 
-        # if we are not starting at the beginning of the trial, some of the bananas may
-        # already be gone. Create them, and then stash them, so the index still refers to
-        # the correct banana
-
-        bananas = range(len(banana_h))
-        self.bananaModel = []
-
-        for i in bananas:
-            #print('i', i)
-            self.bananaModel.append(base.loader.loadModel('../goBananas/models/bananas/banana.bam'))
-            self.bananaModel[i].setPos(
-                Point3(float(banana_pos[i][0]), float(banana_pos[i][1]), float(banana_pos[i][2])))
-            self.bananaModel[i].setScale(0.5)
-            self.bananaModel[i].setH(float(banana_h[i]))
-            self.bananaModel[i].reparentTo(render)
-
-        for i, j in enumerate(self.banana_ts):
-            if j < start_time:
-                self.bananaModel[self.gone_bananas.pop(i)].stash()
-                self.banana_ts.pop(i)
-
-        #self.movie_task = base.movie('../movies/frames/movie_14_2_12', 5, 30, 'png', 4)
-
-        #self.accept("space", base.taskMgr.add, [self.frame_loop, "frame_loop"])
-        self.gameTask = taskMgr.add(self.frame_loop, "frame_loop")
-
-        self.gameTask.last = 0         # Task time of the last frame
-
-        # start the clock at 1 second before the official start so has time to load
-        self.gameTask.game_time = start_time - 100
-        #print('start', self.gameTask.game_time)
-        #print('head start', self.avatar_ht[0])
-        #print('increment', (1 / 60) * 1000000)
+        self.eye_spot = base.loader.loadModel("models/ball")
+        #eye_texture = base.loader.loadTexture('textures/spotlight.png')
+        #self.eye_spot.setTexture(eye_texture, 1)
+        self.eye_spot.setScale(50)
+        self.eye_spot.setTransparency(TransparencyAttrib.MAlpha)
+        self.eye_spot.setColor(1, 1, 1, 0.3)
 
     def frame_loop(self, task):
         dt = task.time - task.last
@@ -200,6 +221,7 @@ class BananaWorld(DirectObject):
     def update_avt_h(self, t_time):
         while self.avatar_ht[0] < t_time:
             if len(self.avatar_ht) == 1:
+                #print('avatar h done')
                 break
             #print(self.avatar_ht[i])
             #print(task.game_time)
@@ -210,6 +232,7 @@ class BananaWorld(DirectObject):
     def update_avt_p(self, t_time):
         while self.avatar_pt[0] < t_time:
             if len(self.avatar_pt) == 1:
+                #print('avatar pos done')
                 break
             #print('move')
             #print(self.avatar_pt[i])
@@ -228,51 +251,80 @@ class BananaWorld(DirectObject):
         # lfp data is taken at 1000Hz, and dt is the number of seconds since
         # the last frame was flipped, so plot number of points = dt * 1000
         lfp = LineSegs()
-        lfp.setThickness(2.0)
-        lfp.moveTo(self.last_lfp_x, 55, self.last_lfp)
+        lfp.setThickness(1.0)
         #print('points to plot', int(dt * 1000))
         self.lfp_test += int(dt * 1000)
         #print('points so far', self.lfp_test)
+
         for i in range(int(dt * 1000)):
             try:
-                self.last_lfp = (next(self.gen_lfp) * self.lfp_gain) + self.lfp_offset
-                self.last_lfp_x += 0.05
-                lfp.drawTo(self.last_lfp_x, 55, self.last_lfp)
+                self.last_lfp.append((next(self.gen_lfp) * self.lfp_gain) + self.lfp_offset)
+                #self.last_lfp_x += 0.05
+                # only plotting 200 data points at a time
+                while len(self.last_lfp) > 2000:
+                    self.last_lfp.pop(0)
             except StopIteration:
                 #print('done with lfp')
                 break
 
-        node = pixel2d.attachNewNode(lfp.create())
-        self.lfp.append(node)
-        # get rid of lfp trace from a while ago..
-        while len(self.lfp) > 500:
+        if self.lfp:
             self.lfp[0].removeNode()
             self.lfp.pop(0)
+        lfp.moveTo(self.last_lfp_x, 55, self.last_lfp[0])
+        x = self.last_lfp_x
+        for i in self.last_lfp:
+            x += .1
+            lfp.drawTo(x, 55, i)
+        node = pixel2d.attachNewNode(lfp.create())
+        self.lfp.append(node)
+
+        # get rid of lfp trace from a while ago..
+        #while len(self.lfp) > 50:
+        #    self.lfp[0].removeNode()
+        #    self.lfp.pop(0)
 
     def update_eye(self, t_time):
-        eye = LineSegs()
-        eye.setThickness(2.0)
+        #eye = LineSegs()
+        #eye.setThickness(10.0)
         #print('last_eye', self.last_eye)
-        eye.moveTo(self.last_eye[0], 55, self.last_eye[1])
+
+        group_eye = []
         while self.last_eye_ts < t_time:
             try:
-                self.last_eye = next(self.gen_eye_pos)
+                group_eye.append(next(self.gen_eye_pos))
                 self.last_eye_ts = next(self.gen_eye_ts)
-                eye.drawTo(self.last_eye[0], 55, self.last_eye[1])
             except StopIteration:
+                #make the next eye movement something crazy in the future
                 self.last_eye_ts = t_time + 10000
                 #print('break')
-                #taskMgr.remove('self.movie_task')
+                taskMgr.remove('self.movie_task')
                 break
-        node = pixel2d.attachNewNode(eye.create())
-        self.eyes.append(node)
+
+        if group_eye:
+            #eye.moveTo(self.last_eye[0], 55, self.last_eye[1])
+            sum_x = 0
+            sum_y = 0
+            for i in group_eye:
+                sum_x += i[0]
+                sum_y += i[1]
+            self.last_eye = [sum_x / len(group_eye), sum_y / len(group_eye)]
+
+            self.eye_spot.setPos(self.last_eye[0], 55, self.last_eye[1])
+            self.eye_spot.reparentTo(pixel2d)
+            #if self.eyes:
+            #    self.eyes[0].removeNode()
+            #    self.eyes.pop(0)
+            #eye.drawTo(self.last_eye[0], 55, self.last_eye[1])
+            #node = pixel2d.attachNewNode(eye.create())
+            #self.eyes.append(node)
         #print(self.eye_data[i][0])
         #print(self.eye_data.pop(i))
 
         # get rid of eye position from a while ago..
-        while len(self.eyes) > 10:
-            self.eyes[0].removeNode()
-            self.eyes.pop(0)
+        #while len(self.eyes) > 1:
+        #    self.eyes[0].removeNode()
+        #    self.eyes.pop(0)
+
 
 class StartError(Exception):
     def __init__(self, message):
